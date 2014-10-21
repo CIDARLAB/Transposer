0bb919c6f12b2671f7597ce67a8fb97ec360b327
Ryan Silva <ryan.jay.silva@gmail.com>
2014-10-21 16:51:42 -0400

    Better comments

--- TweeColi_v0.py
+++ TweeColi_v0.py
@@ -3,8 +3,10 @@ import phScan
 from twython import Twython
 
 print "Welcome to TweeColi!"
-hi = 8				#Set high pH threshold here
-lo = 6					#Set low pH threshold here
+
+# Set high and low pH thresholds
+hi = 8
+lo = 6
 
 # Put your twitter api keys and tokens below
 # Note: The current api keys correspond to @TweeColi 
@@ -12,25 +14,32 @@ apiKey = 'RJSlPyCo084MJKX63tdygPNqZ'
 apiSecret = 'E5i83RLt9VisK4zCngIyHBYxIJx46GD89oowaAiSq4BZeqLXiT'
 accessToken = '2798012371-BYjTww8SShM4lUoh2RNpoAB4TJ1aZLcCQRGZUBc'
 accessTokenSecret = 'neGup8dLJbdlezQb2FrEKNHsxYdQUJCZbDxJ10Iinch7x'
-# This sets up twython
+# Set up twython
 api = Twython(apiKey,apiSecret,accessToken,accessTokenSecret) 
 
+# Initialize pHPre variable, which stores the previous pH reading
 pHPre = 7
+
+# Continuous pH readings
 while True:
 	pH = phScan.readLine()
+	# Test float conversion in case of system response instead of data
 	try:
 		pH = float(pH)
 	except:
 		print "Failed to parse float: ", pH
 		continue
+	# Test for movement from out of steady-state into steady state
   	if(not(hi > pHPre > lo) and (hi > pH > lo)):
 		tweetStr = "Everything is ok! My pH is now %r" % pH
 		api.update_status(status=tweetStr)
 		print "Tweeted: " + tweetStr
+	# Test for movement from steady-state to outside of threshold
 	elif((hi > pHPre > lo) and not(hi > pH > lo)):
 		tweetStr = "Ack! My pH is at %r" % pH
 		api.update_status(status=tweetStr)
 		print "Tweeted: " + tweetStr
+	# Print debugging message
 	else:
 		print "pHPre = %r pH = %r" % (pHPre, pH)
 	pHPre = pH
