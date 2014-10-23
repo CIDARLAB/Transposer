05f007c3c0fe4c877dc25baeb6b530731c435404
Ryan Silva <ryan.jay.silva@gmail.com>
2014-10-23 10:42:22 -0400

    Revert "fixed typo in comment"
    This reverts commit 226f493482c2a14491ba6b377c1a5d2c822b24e4.
    
    Conflicts:
    	TweeColi_v0.py

--- TweeColi_v0.py
+++ TweeColi_v0.py
@@ -22,8 +22,13 @@ pHPre = 7
 
 # Continuous pH readings
 while True:
+<<<<<<< HEAD
 	pH = phScan.get_next_value()
 	# Test float conversion in case of textual system response instead of data
+=======
+	pH = phScan.readLine()
+	# Test float conversion in case of system response instead of data
+>>>>>>> parent of 226f493... fixed typo in comment
 	try:
 		pH = float(pH)
 	except:
