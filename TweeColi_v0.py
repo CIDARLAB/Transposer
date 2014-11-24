037d117ae71542252ca2cf092bcf2d733e17c08a
Ryan Silva <ryan.jay.silva@gmail.com>
2014-11-24 10:55:42 -0500

    typo

--- TweeColi_v0.py
+++ TweeColi_v0.py
@@ -12,14 +12,14 @@ lo = 6
 apiKey = 'fu4svJdREnATr3tNsLEhOvv5b'
 apiSecret = 'uKOhBYw5W3Rlk1RUtPtjo8DERQko8uiQi9l9VddWAU5pAzUZ6e'
 accessToken = '606389094-6HaxmuJZ6cqr4WzuYrBd5uPPWzEqnHar7X184jcv'
-accessTokennSecret = 'GkNQsYZI2L7Zr4prqlgFbgljDHKH6BtpPrOUwuIh9XPY7'
+accessTokenSecret = 'GkNQsYZI2L7Zr4prqlgFbgljDHKH6BtpPrOUwuIh9XPY7'
 
 
 # Keys for @BUBacteria
 #apiKey = 'hbkblS9wvLUroL7tRI6MIO3Hj'
 #apiSecret = 'EwVdnrn1DJzuxuyRDbzgoy1eh6it7x3PMesUoqIaDGstob3FPI'
 #accessToken = '2860939569-oVLMyzz7TeQ3A5ji5WNPROIYQlZZTQ42dThvq7I'
-#accessTokennSecret = 'Op5kNa6MCOTNtSHsoLoXZ4lT5He5JwMvBQOrdJpEqH4je'
+#accessTokenSecret = 'Op5kNa6MCOTNtSHsoLoXZ4lT5He5JwMvBQOrdJpEqH4je'
 
 # Keys for @TweeColi
 #apiKey = 'RJSlPyCo084MJKX63tdygPNqZ'
